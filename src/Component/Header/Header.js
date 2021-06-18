34032785c6409b01385c268ef044735c9f614cde
AlexKostyleff <45436820+AlexKostyleff@users.noreply.github.com>
2021-06-18 19:51:39 +0300

    Демоверсия нового хэдера

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,25 +1,28 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import "./Header.css";
-  
+import ourLogo from "./images/NewLogo.jpg"
 function Header() {
- 
+
+  var a = document.querySelectorAll('.navigation .butt a');
+  for (var i=a.length; i--;) {
+    if (a[i].href === window.location.pathname || a[i].href === window.location.href) a[i].className += ' active';
+  }
     return (
-      <div className="main-header">
-        
-        <NavLink to='/my/test' activeClassName='activeLink'>
-          <button className ="myButton"> Пройти тестирование </button>
-        </NavLink>
-        <NavLink to='/my/institute' activeClassName='activeLink'>
-          <button className ="myButton"> Кафедры </button>
-        </NavLink>
-        <NavLink to='/my/faq' activeClassName='activeLink'>
-          <button className ="myButton"> Техподдержка </button>
-        </NavLink>
-        <NavLink to='/login' activeClassName='activeLink'>
-          <button className ="myButton" >
-            <span className = "icon"></span> Войти </button>
-        </NavLink>
+      <div className="mainHeader">
+      
+         <img className = "OurIcon" src ={ourLogo} alt={"logo"}  width ="72" height="46" left="50" top="45" /> 
+       
+        <div className = "Buttons">
+          <nav className="nav">
+            
+            <ul className="navigation" id="navigation">
+              <li className='butt'> <a className='c' href='/about'>О проекте</a></li>
+              <li className='butt' > <a className='c' href='/departments'>Кафедры</a></li>
+              <li className='butt'><a className='c' href='/login'>Вход</a></li>
+              <li className='butt'><a className='Registrationbutton' href='/signin'>Регистрация</a></li>
+            </ul>
+          </nav>
+        </div>
       </div>
     );
   }
