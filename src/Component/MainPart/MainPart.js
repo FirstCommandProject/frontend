b21aca83f93cc5b76f8b4409363ba2facb9e5434
DanilSvechnikar <https://github.com/DanilSvechnikar>
2021-06-20 15:44:08 +0300

    Хз

--- src/Component/MainPart/MainPart.js
+++ src/Component/MainPart/MainPart.js
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 import MainPicture from '../../Source/images/main-img.png';
 import Header from "../Header/Header";
 import './MainPart.css';
